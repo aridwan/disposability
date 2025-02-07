b3a7017bdc093f4af94cc2e72c4769f5d1dcb712
M. Arief Ridwan <aridwan05@gmail.com>
2025-02-07 17:52:32 +0700

    add pending task counter for graceful shutdown

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var isReady = false;
 var isStarted = false;
 var isHealthy = false;
 
+var pendingTasks = 0;
+
 // Initialize connections
 async function initConnections() {
   try {
@@ -32,7 +34,8 @@ async function initConnections() {
     // MongoDB client
     const mongoClient = new MongoClient(MONGO_URL);
     await mongoClient.connect();
-    mongoDb = mongoClient.db();
+    mongoDb = mongoClient.db("mydb");
+    
     console.log('Connected to MongoDB');
 
     // Kafka producer
@@ -67,7 +70,9 @@ async function closeConnections() {
     // Close Kafka producer
     await kafkaProducer.disconnect();
     console.log('Kafka producer connection closed.');
-
+    isReady = false;
+    isStarted = false;
+    isHealthy = false;
   } catch (err) {
     console.error('Error closing connections:', err);
   }
@@ -78,7 +83,51 @@ app.get('/health', handleHealthCheck);
 app.get('/liveness', handleLivenessCheck);
 app.get('/readiness', handleReadinessCheck);
 app.get('/startup', handleStartupCheck);
+app.get('/long-process', handleLongProcess);
+app.post('/insert-mongo', handleInsertMongo);
 app.get('/', (req, res) => res.send('Hello, World!'));
+
+// Simulate long process
+async function handleLongProcess(req, res) {
+  try {
+    pendingTasks++;
+    const result = await pool.query('SELECT pg_sleep(10)');
+    pendingTasks--;
+    res.status(200).json({ message: 'Long process completed' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Long process failed');
+  }
+}
+
+// Simulate sleep for 10 seconds
+function simulateSleep() {
+  const start = Date.now();
+  pendingTasks++;
+  console.log('Sleeping... 10 seconds');
+  while (Date.now() - start < 10000) {
+    // Do nothing
+  }
+  pendingTasks--;
+}
+
+// Insert a document in MongoDB
+async function handleInsertMongo(req, res) {
+  try {
+    simulateSleep();
+    console.log('Connected successfully to MongoDB');
+
+    const collection = mongoDb.collection('users');
+
+    const result = await collection.insertOne({ "test": "value" });
+    console.log('Inserted document with ID:', result.insertedId);
+
+    res.status(201).json({ message: `Document inserted with ID: ${result.insertedId}` });
+  } catch (err) {
+    console.error('Error inserting document:', err);
+    res.status(500).json({ error: 'Error inserting document' });
+  }
+}
 
 // Health Check Handlers
 async function handleHealthCheck(req, res) {
@@ -162,7 +211,7 @@ async function handleReadinessCheck(req, res) {
 // Startup Check - Ensures all services are successfully started
 function handleStartupCheck(req, res) {
   // Check if all services are connected
-  if (pool && redisClient && mongoDb ) { //&& kafkaProducer) {
+  if (pool && redisClient && mongoDb && kafkaProducer) {
 
     isStarted = true;
     res.status(200).send('Service has successfully started');
@@ -174,14 +223,20 @@ function handleStartupCheck(req, res) {
 // Gracefully handle process exit to close connections
 process.on('SIGTERM', async () => {
   console.log('Received SIGTERM, shutting down gracefully...');
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  while (pendingTasks > 0) {
+    console.log(`Waiting for ${pendingTasks} task(s) to finish...`);
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second before checking again
+  }
   await closeConnections();
   process.exit(0);
 });
 
 process.on('SIGINT', async () => {
-  console.log('Received SIGINT, shutting down gracefully...');
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  console.log('Received SIGTERM, shutting down gracefully...');
+  while (pendingTasks > 0) {
+    console.log(`Waiting for ${pendingTasks} task(s) to finish...`);
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second before checking again
+  }
   await closeConnections();
   process.exit(0);
 });
